861067571f8311083bbf22535c79d32615a5957f
suntaeyoung <sty8706@naver.com>
2024-01-15 16:28:42 +0900

    1주차 1번째 과제

--- week1-assignment1/main.js
+++ week1-assignment1/main.js
@@ -1,22 +1,6 @@
-// 기본 복사
-// const a = { x : 1};
-// const b = a;
+// 1. 자바스크립트 함수 연습 - 객체의 복사
 
-// b.x = 2;
-
-// console.log(a);
-// console.log(b);
-
-// 얕은 복사
-// const c = { x : 1};
-// const d = { ...c };
-
-// d.x = 3;
-
-// console.log(c);
-// console.log(d);
-
-
+// cloneDeep 함수를 라이브러리 없이 구현해보세요.
 // 깊은 복사
 const obj = {
   x : 1,
@@ -45,5 +29,27 @@ copyDeep.y.z = 3;
 console.log(obj);
 console.log(copyDeep);
 console.log(obj.y.z === copyDeep.y.z);
+
+
+
+
+// 기본 복사
+// const a = { x : 1};
+// const b = a;
+
+// b.x = 2;
+
+// console.log(a);
+// console.log(b);
+
+// 얕은 복사
+// const c = { x : 1};
+// const d = { ...c };
+
+// d.x = 3;
+
+// console.log(c);
+// console.log(d);
+
 
 
