ed80de0cd2209b1102d2ea135543dd94adc80dc2
suntaeyoung <sty8706@naver.com>
2024-01-22 11:24:07 +0900

    1주차 3번과제 수정

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-// 2. 자바스크립트 함수 연습 - 효율성
+// 3. 자바스크립트의 객체
 
-// memoize 함수를 라이브러리 없이 구현해보세요.
-function memoize(func) {
-  const cache = {};
-
-  return function (...args) {
-    // 함수의 인자들을 문자열로 받아서 키로 사용
-    const key = JSON.stringify(args);
-
-    // 캐시에 해당 키에 대한 값이 이미 존재한다면 캐시에서 값을 가져와서 반환
-    return cache[key] || (cache[key] = func(...args));
-  }
+// 오브젝트를 생성하는 세가지 방법에 대해 공부하고 각각에 대한 예시 코드를 작성해주세요.
+// 객체 리터럴
+const obj1 = {
+  name: 'Sun',
+  age: 28,
+  gender: 'male',
 }
 
-const slowFunction = (n) => {
-  let result = 0;
+console.log(obj1);
 
-  for(let i = 1; i <= n; i++) {
-    for(let j = 1; j <= n; j++) {
-      result += 1;
-    }
-  }
 
-  return result;
+// new
+const obj2 = new Object();
+obj2.name = 'apple';
+obj2.price = 2000;
+obj2.color = 'red';
+
+console.log(obj2);
+
+
+// 함수
+function Fruit (name, price, color) {
+  this.name = name;
+  this.price = price;
+  this.color = color;
 }
-
-const memoizeFunction = memoize(slowFunction);
-
-console.log(memoizeFunction(10));
-console.log(memoizeFunction(10));
+const fruit = new Fruit('banana', 1000, 'yellow');
+console.log(fruit);
