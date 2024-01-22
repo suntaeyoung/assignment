46a57cce213bb5334b3bb657de08132cd752b361
suntaeyoung <sty8706@naver.com>
2024-01-22 11:20:44 +0900

    1주차 2번과제 수정

--- main.js
+++ main.js
@@ -1,34 +1,31 @@
-// 1. 자바스크립트 함수 연습 - 객체의 복사
+// 2. 자바스크립트 함수 연습 - 효율성
 
-// cloneDeep 함수를 라이브러리 없이 구현해보세요.
-const originalObj = {
-  x: 1,
-  y: {
-    z: 2,
+// memoize 함수를 라이브러리 없이 구현해보세요.
+function memoize(func) {
+  const cache = {};
+
+  return function (...args) {
+    // 함수의 인자들을 문자열로 받아서 키로 사용
+    const key = JSON.stringify(args);
+
+    // 캐시에 해당 키에 대한 값이 이미 존재한다면 캐시에서 값을 가져와서 반환
+    return cache[key] || (cache[key] = func(...args));
   }
 }
 
-function cloneDeep(obj) {
-  // 기본형일때
-  if(typeof obj !== 'object'){
-    return obj;
+const slowFunction = (n) => {
+  let result = 0;
+
+  for(let i = 1; i <= n; i++) {
+    for(let j = 1; j <= n; j++) {
+      result += 1;
+    }
   }
 
-  // 배열일때
-  if(Array.isArray(obj)){
-    return obj.map(cloneDeep);
-  }
-
-  // 객체일때
-  return Object.keys(obj).reduce((acc, cur) => {
-    acc[cur] = cloneDeep(obj[cur]);
-    return acc
-  }, {})
+  return result;
 }
 
-const cloneObj = cloneDeep(originalObj);
+const memoizeFunction = memoize(slowFunction);
 
-cloneObj.y.z = 3;
-
-console.log(originalObj);
-console.log(cloneObj);
+console.log(memoizeFunction(10));
+console.log(memoizeFunction(10));
